8d638948e33afac2a8c1d927d47efcd8cb5349ff
Yu Bi <790125037@qq.com>
2019-07-22 14:04:27 +0800

    :bento: 添加店铺前端设计
    使用SUI Mobile快速搭建html网页

---
+++ src/main/java/com/biyu/o2o/web/shopadmin/ShopAdminController.java
@@ -0,0 +1,19 @@
+package com.biyu.o2o.web.shopadmin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "shopadmin", method = {RequestMethod.GET})
+public class ShopAdminController {
+
+    /**
+     * 
+     * @return
+     */
+    @RequestMapping(value = "/shopoperation")
+    public String shopOperation() {
+        return "shop/shopoperation";
+    }
+}
